261bcedf24a00ff9cf911348bb19e988ee6bec5e
Avinash Singh <avinashsingh@Avinashs-MacBook-Pro-2.local>
2017-04-23 19:02:13 -0400

    Insurance Data

--- Health/src/main/webapp/app/controller/dataframe/InsuranceDataSet.js
+++ Health/src/main/webapp/app/controller/dataframe/InsuranceDataSet.js
@@ -8,12 +8,47 @@ class InsuranceDataSet extends DataSet{
     }
     
 	getColumnList() {
+		var DataFrame = dfjs.DataFrame;
+        return DataFrame.fromCSV('./DownloadableContent/HealthInsurance.csv').then(
+            df => {
+                var columns = df.listColumns();
+                return columns;
+            }
+        ).catch(err => {
+            console.log(err);
+        });
 	}
 	
 	getDistinctColumnVal() {
+		var DataFrame = dfjs.DataFrame;
+		var distColumnValues = DataFrame.fromCSV('./DownloadableContent/HealthInsurance.csv').then(
+            df => {
+                var columns = df.listColumns();
+                var distColumnValues = [];
+
+                for (var i=0; i<columns.length; i++){
+                    distColumnValues[i] = df.distinct(columns[i]).toDict()[columns[i]];
+                }
+
+                return distColumnValues;
+            }
+        ).catch(err => {
+        console.log(err);
+        });
+        return distColumnValues;
 	}
 
 	getTableData(){
+		var DataFrame = dfjs.DataFrame;
+		var tableData = DataFrame.fromCSV('./DownloadableContent/HealthInsurance.csv').then(
+            df => {
+                var data = df.toArray();
+                return data;
+            }
+        ).catch(err => {
+            console.log(err);
+        });
+        return tableData;
 	}
 	
 	getQueryData(){
